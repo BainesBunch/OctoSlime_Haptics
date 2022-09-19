1e252b1f0fea63ee58f64ada9e649a3872a9cd70
EmbeddedAT <Peter@EmbeddedAT.com>
2022-09-19 19:51:37 +0200

    removed surplus libs

--- Firmware/Master/src/debug.h
+++ Firmware/Master/src/debug.h
@@ -35,13 +35,7 @@
 #define serialBaudRate 115200
 #define UPDATE_IMU_UNCONNECTED 1
 #define SEND_UPDATES_UNCONNECTED 1
-#define LED_INTERVAL_STANDBUY 10000
 #define STATUS_PRINT_INTERVAL 15000
-#define ENABLE_LEDS true
-
-#define LOADING_LED D6
-#define CALIBRATING_LED D7
-#define STATUS_LED D8
 
 // Determines how often we sample and send data
 #define samplingRateInMillis 12
@@ -69,7 +63,7 @@
 //#define OPTIMIZE_UPDATES false
 
 
-#define I2C_SPEED 80000
+#define I2C_SPEED 40000
 
 #define FIRMWARE_BUILD_NUMBER 1
 #define FIRMWARE_VERSION "1.1.0"
